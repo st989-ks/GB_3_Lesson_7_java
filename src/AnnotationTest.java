3ae087457dfe1a37386762f7a75431c86fe82504
galaxy1 <st989@ya.ru>
2020-11-10 20:41:51 +0400

    Initial commit

---
+++ src/AnnotationTest.java
@@ -0,0 +1,20 @@
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@interface BeforeSuite {
+}
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@interface AfterSuite {
+}
+
+@Retention (RetentionPolicy.RUNTIME)
+@Target (ElementType.METHOD)
+@interface Test {
+    int priority() default 5;
+}
---
+++ src/TestImpl.java
@@ -0,0 +1,2 @@
+public class TestImpl {
+}
